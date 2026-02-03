716fc427a3d1c9594a98ea85466ab745420e98fe
sharmaakash-198 <122460958+sharmaakash-198@users.noreply.github.com>
2026-02-03 10:36:38 +0530

    Imported Vercel's SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <SpeedInsights />
     </div>
   )
 }
