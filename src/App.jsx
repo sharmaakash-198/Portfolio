c5fb4bbfc2d65c665da3fb1c448b55363b1df275
sharmaakash-198 <122460958+sharmaakash-198@users.noreply.github.com>
2026-02-03 10:43:46 +0530

    Fallback to previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
         <Contact />
       </main>
       <Footer />
-      <SpeedInsights />
     </div>
   )
 }
